5a7f113b3eef76ee95998b64d07e55889596181d
Glutinfree <119464101+A2Bocanegra@users.noreply.github.com>
2023-02-02 17:16:18 -0800

    started scoring commands
    a lot of these still needs some work, this is all foundational.

---
+++ src/main/java/frc/robot/commands/Base/getScoringReady.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Base;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Scoring;
+public class getScoringReady extends CommandBase {
+  Scoring scoring = new Scoring();
+
+  public getScoringReady(Scoring scoring) {
+    this.scoring = scoring;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() 
+  {
+    scoring.moveClawMotors(0); //establish a constant for this   
+  }
+
+  
+  @Override
+  public void execute() //if the game object has been picked up, claw closes
+  {
+    if(true/*establish if game object has been picked up*/){
+        scoring.moveClawMotors(0); //establish another constant for this. only use if the game object hasn't been picked up
+        scoring.moveExtensionMotors(0); //establish a constant for this
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
