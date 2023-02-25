eef0b99b9c44d01b0870dd9343b7dbdf5458447b
Glutinfree <117413359+JAKlein26@users.noreply.github.com>
2023-02-24 17:13:57 -0800

    deleted a single letter for Joshua D

--- src/main/java/frc/robot/subsystems/Orientation.java
+++ src/main/java/frc/robot/subsystems/Orientation.java
@@ -35,7 +35,7 @@ public class Orientation extends SubsystemBase {
 
     private boolean orientationMode;
     
-    public KORIENTATIONSTATE OrientationState = KORIENTATIONSTATE.CUBE;
+    public ORIENTATIONSTATE OrientationState = ORIENTATIONSTATE.CUBE;
 
     public Orientation() {
 
