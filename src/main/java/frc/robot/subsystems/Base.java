98e6e1cefc402a26628f2dbcc702331923cad7c7
JAKlein26 <74387887+Patrick62@users.noreply.github.com>
2023-02-28 19:32:46 -0800

    needs testing + sysid before merge

--- src/main/java/frc/robot/subsystems/Base.java
+++ src/main/java/frc/robot/subsystems/Base.java
@@ -3,7 +3,10 @@ package frc.robot.subsystems;
 import static frc.robot.Constants.*;
 
 import com.kauailabs.navx.frc.AHRS;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -13,6 +16,9 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Base extends SubsystemBase {
@@ -94,7 +100,47 @@ public class Base extends SubsystemBase {
     frontRightModule.setDesiredState(states[1]);
     backLeftModule.setDesiredState(states[2]);
     backRightModule.setDesiredState(states[3]);
-}
+  }
+  
+  public void resetOdometry(Pose2d pose) {
+    resetAllRelEncoders();
+    this.pose = pose;
+    
+    odometry.resetPosition(getHeading(), getPositions(), pose);
+  }
+
+  public Command followTrajectoryCommand(PathPlannerTrajectory traj, boolean isFirstPath) {
+    return new SequentialCommandGroup(
+         new InstantCommand(() -> {
+           // Reset odometry for the first path you run during auto
+           if(isFirstPath){
+              resetOdometry(traj.getInitialHolonomicPose());
+           }
+         }),
+         new PPSwerveControllerCommand(
+             traj,
+             this::getPose, // Pose supplier
+             this.kinematics, // SwerveDriveKinematics
+             new PIDController(2, 0, 0), // X controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
+             new PIDController(2, 0, 0), // Y controller (usually the same values as X controller)
+             new PIDController(0, 0, 0), // Rotation controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
+             this::setModuleStates, // Module states consumer
+             true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
+             this // Requires this drive subsystem
+         )
+     );
+  }
+
+  public Pose2d getPose() {
+    return pose;
+  }
+
+  public void setModuleStates(SwerveModuleState[] desiredStates) {
+    frontLeftModule.setDesiredState(desiredStates[0]);
+    frontRightModule.setDesiredState(desiredStates[1]);
+    backLeftModule.setDesiredState(desiredStates[2]);
+    backRightModule.setDesiredState(desiredStates[3]);
+  }
 
   // recalibrates gyro offset
   public void resetGyro() {
