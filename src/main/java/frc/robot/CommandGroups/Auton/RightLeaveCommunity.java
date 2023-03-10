57d94a2eb3696d652c80526904e4dc1199f6691c
Glutinfree <111544394+Glutinfree@users.noreply.github.com>
2023-03-09 20:09:10 -0800

    stuff

--- src/main/java/frc/robot/CommandGroups/Auton/RightLeaveCommunity.java
+++ src/main/java/frc/robot/CommandGroups/Auton/RightLeaveCommunity.java
@@ -2,24 +2,24 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.CommandGroups;
-
-import frc.robot.commands.Base.AutoBalance;
-import frc.robot.subsystems.Base;
+package frc.robot.CommandGroups.Auton;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.Auton.FollowPath;
+import frc.robot.subsystems.Base;
+import static frc.robot.Constants.*;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoBalanceAuton extends SequentialCommandGroup {
-  // private Base base;
-  /** Creates a new AutoBalanceAuton. */
-  public AutoBalanceAuton(Base base) {
+public class RightLeaveCommunity extends SequentialCommandGroup {
+  /** Creates a new RightLeaveCommunity. */
+  Base base;
+  public RightLeaveCommunity(Base base) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new AutoBalance(base)
+      new FollowPath(base, KRightSideLeaveCommunity, isFinished())
     );
   }
 }
